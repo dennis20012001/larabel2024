cb1fa718ee01828f915345a73b766b20c74d9307
Javi <in2dm3-v@elorrieta.int>
2024-11-07 19:42:55 +0100

    Segundo commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('created_at')->get();
+        return view('posts.index',['posts' => $posts]);
     }
 
     /**
@@ -36,7 +37,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('posts.show',['post'=>$post]);
     }
 
     /**
