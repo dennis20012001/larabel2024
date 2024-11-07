caf236bd7b76c28580532d1e4d98469722f89c5a
Javi <in2dm3-v@elorrieta.int>
2024-11-07 16:20:57 +0100

    primer commit

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder {
+    public function run(): void {
+        DB::table('posts')->insert([
+            "titulo"=>"Primer post",
+            "texto"=>"Este es el texto del primer post",
+            "publicado"=>true,
+            "created_at"=>now()
+        ]);
+    }
+}
